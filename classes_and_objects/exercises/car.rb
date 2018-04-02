c5869b975ad23ca494eafbf97771e0709ea72898
kuldeep-surendra <ksurendra@qwinix.io>
2018-04-02 14:38:59 +0530

    exercises

---
+++ classes_and_objects/exercises/car.rb
@@ -0,0 +1,6 @@
+# Create a class called Car
+# Create a Constructor and initialize an object
+# Create the following methods mentioned below
+# start -> display the message "car is started".
+# stopped -> display the message "car is stopped".
+# move -> display the message "car is in motion".
